979d3881daa4290a8039495d4758fdd24dc776a6
Katrodiya Archil <73387028+katrodiyaarchil@users.noreply.github.com>
2021-04-02 00:19:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import cv2
 import functions
+import database
 
 # Read the image
 img = cv2.imread('taken.jfif')
 
 # Segment the leaf
-segmented = functions.segmentation(img)
+seg1 = functions.segmentation(img)
+seg2 = functions.segmentation1(img)
+
+# Compare both segmentation to get most appropriate one
+segmented,final = functions.compareTwo(seg1,seg2)
 
 # Apply contrasting
 contrasted_img = functions.contrasting(segmented)
@@ -17,6 +22,15 @@ hsi= functions.RGB_TO_HSI(contrasted_img)
 damaded_only = functions.damaged_only(contrasted_img)
 
 hsi2=functions.RGB_TO_HSI(damaded_only)
+
+# Finding damaged part into database
+count = cv2.countNonZero(dmaged_only)
+Intensity = np.mean(damaged_only)
+print("Average Intensity   :",round(Intensity,3))
+print("Damaged Area Approx :",round((count*100/final),2),"%","\n")
+print(Intensity)
+database.disease(Intensiy)
+
 
 cv2.imshow('original',img)
 cv2.imshow('segmented',segmented)
